e385d1655143020670c039f42bda94ce5e8f0bf5
AlefeVT <alefevt@gmail.com>
2024-10-21 11:38:01 -0300

    feat: fix stripe success page

--- src/app/(dashboard)/callback/stripe/success/page.tsx
+++ src/app/(dashboard)/callback/stripe/success/page.tsx
@@ -1,12 +1,10 @@
-import InfoBar from '@/components/infobar';
 import React from 'react';
 
 type Props = {};
 
-const Page = async (props: Props) => {
-  return (
+const Page = (props: Props) => {
+  return (  
     <>
-      <InfoBar />
       <div className="overflow-y-auto w-full chat-window flex-1 h-0">
         Conta Conectada com sucesso!
       </div>
